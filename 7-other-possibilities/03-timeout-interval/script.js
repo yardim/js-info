1457cc54408a1850fdedccb9bd32934fba701c66
Dmytro Yaroshenko <dmytro.yaroshenko@do.local>
2019-05-07 16:30:18 +0300

    7-03 - timeout and intervals

---
+++ 7-other-possibilities/03-timeout-interval/script.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// ================================
+// print numbers per interval
+
+// function printNumbersWithInterval() {
+//   let number = 1;
+
+//   let intervalId = setInterval(() => {
+//     if (number === 20) {
+//       clearInterval(intervalId);
+//     }
+
+//     console.log(number++);
+//   }, 100);
+// }
+
+// printNumbersWithInterval();
+
+
+
+// ================================
+// print numbers with timeout
+
+// function printNumbersWithInterval() {
+//   let number = 1;
+//   setTimeout(printNumber, 100);
+
+//   function printNumber() {
+//     if (number > 20) {
+//       return;
+//     }
+
+//     console.log(number++);
+//     setTimeout(printNumber, 100);
+//   }
+// }
+
+// printNumbersWithInterval();
+
+
+
+// =================================
+// delay function
+
+// function delay(f, delay) {
+//   return function() {
+//     setTimeout(() => f.apply(this, arguments), delay);
+//   }
+// }
+
+// function f(x, y) {
+//   console.log(x, y);
+// }
+
+// const f1000 = delay(f, 1000);
+// const f1500 = delay(f, 1500);
+
+// f1000('test', 'test 2');
+// f1500('test 3', 'test 4');
+
+
+
+// =================================
+// debounce
+
+function debounce(f, ms) {
+  let timeoutId;
+
+  return function() {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      f.apply(this, arguments);
+    }, ms);
+  }
+}
+
+function test(param) {
+  console.log(param);
+}
+
+const debouncedTest = debounce(test, 1000);
+debouncedTest('test 1');
+debouncedTest('test 2');
+
+setTimeout(() => {
+  debouncedTest('test 3');
+}, 900);
+
+setTimeout(() => {
+  debouncedTest('test 4');
+}, 1500);
+
